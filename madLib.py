2476b027c01a543f3191da1ae0bf1876742adcad
Nkandu Mukabe <ncmukabe@gmail.com>
2024-08-16 16:15:42 +0200

    Initial commit

---
+++ madLib.py
@@ -0,0 +1,44 @@
+# intro
+
+print("Welcome to MadLits! The poetry madlib game!\n")
+name = input("Enter your name: ")
+print("Great! Let's get started!")
+
+# inputs
+var1 = input("Enter an adjective: ")
+var2 = input("Enter a noun, position: ")
+var3 = input("Enter a time of day: ")
+var4 = input("Enter an adjective: ")
+var5 = input("Enter a verb: ")
+var6 = input("Enter a past-tense verb: ")
+var7 = input("Enter another past-tense verb: ")
+var8 = input("Enter a weapon: ")
+var9 = input("One more past-tense verb: ")
+var10 = input("Enter a noun: ")
+var11 = input("Enter an adjective: ")
+var12 = input("Enter another adjective: ")
+var13 = input("One last past-tense verb I swear: ")
+
+# printout!
+print()
+print("One " + var1 + " day in the " + var2 + " of the " + var3)
+print("two " + var4 + " people got up to " + var5)
+print("back to back as they " + var6 + " each other, ")
+print(var7 + " their " + var8 + " and " + var9 + " each other.")
+print("if you don't believe this " + var10 + " is " + var11)
+print("ask the " + var12 + " guy he " + var13 + " it too")
+print()
+
+# thanks and acknowledgements
+print("Thanks for playing MadLits " + name + "! Come again soon!")
+print()
+print("Original poem: Anonymous")
+
+#Future improvements:
+      #be able to store an unlimited number of vars as the poem requires
+      #find a way to add more poems and select one
+      #improve the way the final print is coded
+
+
+
+
